12ea88dd464647c93f8537bda6e53f98eafe0223
lucavise <luca.visentin@siav.it>
2022-07-21 10:12:48 +0200

    lv - primo commit

---
+++ modules/mvctest/src/main/java/Mvctest/constants/MvctestPortletKeys.java
@@ -0,0 +1,11 @@
+package Mvctest.constants;
+
+/**
+ * @author lvisentin@siav.net
+ */
+public class MvctestPortletKeys {
+
+	public static final String MVCTEST =
+		"Mvctest_MvctestPortlet";
+
+}
---
+++ modules/mvctest/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,3 @@
+<%@ include file="/init.jsp" %>
+
+<div class="">Ciao a tutti</div>
